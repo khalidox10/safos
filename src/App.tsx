5dbc516730c70aa682998f6aab17ef1efeddcaf6
khalidox10 <khalidaghenda10@gmail.com>
2026-06-07 02:26:40 +0200

    Add HomePage component and update routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,24 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { StoreProvider } from './context/StoreContext';
 import AdminDashboard from './pages/AdminDashboard';
 import ProductPage from './pages/ProductPage';
+import HomePage from './pages/HomePage'; // استيراد الصفحة الرئيسية الفاخرة للزبناء
 
-// المكون الرئيسي للتوجيه والربط
 export default function App() {
   return (
     <StoreProvider>
       <Router>
         <Routes>
+          {/* 🟢 الصفحة الرئيسية الفاخرة لمتجر SAFOS للحقائب المطرزة */}
+          <Route path="/" element={<HomePage />} />
+          
           {/* مسار لوحة التحكم الإدارية الشاملة */}
           <Route path="/admin" element={<AdminDashboard />} />
           
           {/* مسار صفحة المنتج المصممة بأسلوب Shopify للزبناء */}
           <Route path="/product/:id" element={<ProductPage />} />
           
-          {/* التوجيه الافتراضي لأي رابط خاطئ */}
-          <Route path="*" element={<Navigate to="/admin" replace />} />
+          {/* التوجيه التلقائي لأي رابط غير معروف إلى الصفحة الرئيسية */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </StoreProvider>
